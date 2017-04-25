9d7322f002c4e1e9b87d6a09f2b680e971d2534e
Venkat <venkat.manchikalapudi@logianalytics.com>
2017-04-25 14:43:33 -0400

    more updates.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# vibrentChallenge
+1. Angular app with a directive for a Form with an input box.
+2. The template for the directive is included in calculateEuation.tpl.html.
+3. Form has validation to show an error message when illegal charaters are entered.
+4. Angular controller with a watch on the input box's model with a debounce.
+5. Added an Angular service that implements a mathematical equation parser that parses simple equations.
+6. The angular service is injected in to the controller which triggers the calculations.
--- script.js
+++ script.js
@@ -1,104 +1,98 @@
-var vibrentApp = angular.module('vibrentMod', []);
-vibrentApp.directive('vibrentChallenge', [function () {
+var calculateEquationApp = angular.module('calculateEquationMod', []);
+calculateEquationApp.directive('calculateEquation', [function () {
     return {
         restrict: 'E',
-        controller: 'vibrentChallengeCtrl',
-        templateUrl: 'vibrentChallenge.tpl.html',
+        controller: 'calculateEquationCtrl',
+        templateUrl: 'calculateEquation.tpl.html',
         scope: {},
         link: function (scope, ele, attrs) {
         }
     }
 }]);
 
-vibrentApp.controller('vibrentChallengeCtrl', ['$scope', 'vibrentEquationParserSvc', function ($scope, vibrentEquationParserSvc) {
+calculateEquationApp.controller('calculateEquationCtrl', ['$scope', 'calculateEquationParserSvc', function ($scope, calculateEquationEquationParserSvc) {
     $scope.$watch('txtInput', _.debounce(function (newVal, oldVal) {
-        if ($scope.vibrentChallengeForm.$valid) {
+        console.log(newVal);
+        if ($scope.calculateEquationForm.$valid) {
             if (newVal !== oldVal) {
                 $scope.$apply(function () {
-                    $scope.output = vibrentEquationParserSvc.solveStr(newVal, $scope);
+                    $scope.output = calculateEquationEquationParserSvc.solveStr(newVal, $scope);
                 })
             }
         }
-    }), 1000);
+    }, 1000));
 }]);
 
-vibrentApp.service('vibrentEquationParserSvc', function () {
-    //https://www.apptic.me/blog/evaluating-mathematical-expression-javascript.php
-    function replaceAll(haystack, needle, replace) {
-        return haystack.split(needle).join(replace);
-    } // replace all fx;
+calculateEquationApp.service('calculateEquationParserSvc', function () {
 
-    function strContain(haystack, needle) {
-        return haystack.indexOf(needle) > -1;
-    } // custom true/false contains
-
-    function isParseable(n, minus) {
-        return (!isNaN(n) || (n == "-" && !minus) || n == ".");
-    } // determine if char should be added to side
-
-    function getSide(haystack, middle, direction, minus) {
-        var i = middle + direction;
-        var term = "";
-        var limit = (direction == -1) ? 0 : haystack.length; // set the stopping point, when you have gone too far
-        while (i * direction <= limit) { // while the current position is >= 0, or <= upper limit
-            if (isParseable(haystack[i], minus)) {
-                if (direction == 1) term = term + haystack[i];
-                else term = haystack[i] + term;
-                i += direction;
-            } else {
-                return term;
-            }
-        }
-        return term;
-    } // general fx to get two terms of any fx (multiply, add, etc)
-
-    function allocFx(eq, symbol, alloc, minus) {
-        minus = (typeof minus !== 'undefined'); // sometimes we want to capture minus signs, sometimes not
-        if (strContain(eq, symbol)) {
-            var middleIndex = eq.indexOf(symbol);
-            var left = getSide(eq, middleIndex, -1, minus);
-            var right = getSide(eq, middleIndex, 1, false);
-            eq = replaceAll(eq, left + symbol + right, alloc(left, right));
-        }
-        return eq;
-    } // fx to generically map a symbol to a function for parsing
-
-    this.solveStr = function (eq, scope) {
-        eq = reformat(eq);
-        while (strContain(eq, "*") || strContain(eq, "/")) {
-            var multiply = true;
-            if (eq.indexOf("*") < eq.indexOf("/")) {
-                multiply = (strContain(eq, "*"));
-            } else {
-                multiply = !(strContain(eq, "/"));
-            }
-            eq = (multiply) ? allocFx(eq, "*", function (l, r) {
-                return parseFloat(l) * parseFloat(r);
-            }) : allocFx(eq, "/", function (l, r) {
-                return parseFloat(l) / parseFloat(r);
-            });
-        }
-        while (strContain(eq, "+")) eq = allocFx(eq, "+", function (l, r) {
-            return parseFloat(l) + parseFloat(r);
-        });
-        return isParseable(eq) ? eq : "Finish the equation";
+    function bStrContains(baseStr, splitter) {
+        return baseStr.indexOf(splitter) > -1;
     };
 
-    function reformat(s) {
-        s = s.toLowerCase();
-        s = replaceAll(s, "-(", "-1*(");
-        s = replaceAll(s, ")(", ")*(");
-        s = replaceAll(s, " ", "");
-        s = replaceAll(s, "-", "+-");
-        s = replaceAll(s, "--", "+");
-        s = replaceAll(s, "++", "+");
-        s = replaceAll(s, "(+", "(");
-        for (var i = 0; i < 10; i++) {
-            s = replaceAll(s, i + "(", i + "*" + "(");
+    function getPortionStr(baseStr, baseIndex, moveIndexBy, minus) {
+        var i = baseIndex + moveIndexBy;
+        var strSideVal = "";
+        var limit = (moveIndexBy == -1) ? 0 : baseStr.length;
+        while (i * moveIndexBy <= limit) {
+            //move as long as parseable, then stop and return.
+            if (!isNaN(baseStr[i], minus)) {
+                if (moveIndexBy == 1) strSideVal = strSideVal + baseStr[i];
+                else strSideVal = baseStr[i] + strSideVal;
+                i += moveIndexBy;
+            } else {
+                return strSideVal;
+            }
         }
-        while(s.charAt(0) == "+") s = s.substr(1);
-        return s;
-    } // standardize string format
+        return strSideVal;
+    };
+
+    function resolveEquationFunc(equation, operator, resolveFunc) {
+        //minus = (typeof minus !== 'undefined');
+        if (bStrContains(equation, operator)) {
+            var middleIndex = equation.indexOf(operator);
+            var left = getPortionStr(equation, middleIndex, -1);
+            var right = getPortionStr(equation, middleIndex, 1);
+            equation = replaceStrFunc(equation, left + operator + right, resolveFunc(left, right));
+        }
+        return equation;
+    };
+
+    this.solveStr = function (equation) {
+        // format the equation
+        equation = equation.toLowerCase();
+        equation = replaceStrFunc(equation, " ", "");
+        equation = replaceStrFunc(equation, "-", "+-");
+        equation = replaceStrFunc(equation, "--", "+");
+
+        while (bStrContains(equation, "*") || bStrContains(equation, "/")){
+        // solve multiplication followed by division and the addition/substraction
+            var doMultiplyFirst = true;
+            if (equation.indexOf("*") < equation.indexOf("/")) {
+                doMultiplyFirst = (bStrContains(equation, "*"));
+            } else {
+                doMultiplyFirst = !(bStrContains(equation, "/"));
+            }
+
+            if(doMultiplyFirst){
+                equation = resolveEquationFunc(equation, "*", function (leftStr, rightStr) {
+                    return parseFloat(leftStr) * parseFloat(rightStr);
+                });
+            }else{
+                equation = resolveEquationFunc(equation, "/", function (leftStr, rightStr) {
+                    return parseFloat(leftStr) / parseFloat(rightStr);
+                });
+            }
+        }
+        while (bStrContains(equation, "+")) equation = resolveEquationFunc(equation, "+", function (l, r) {
+            return parseFloat(l) + parseFloat(r);
+        });
+        return !isNaN(equation) ? equation : "Finish the equation";
+    };
+
+    function replaceStrFunc(baseStr, splitter, replacement) {
+        var splitVal = baseStr.split(splitter);
+        return splitVal.join(replacement);
+    };
 
 })
 
